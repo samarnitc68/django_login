2a0c6b018a0611912f7afed5ee12d29618317c8e
samarnitc68 <spatel@innominds.com>
2021-12-16 11:14:11 +0530

    second commit

--- home/urls.py
+++ home/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('home.urls'))
+   path('',views.index, name = 'home'),
+   path('login',views.loginUser, name = 'login'),
+   path('logout',views.logoutUser, name = 'logout')
 ]
